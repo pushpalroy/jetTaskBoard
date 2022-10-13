c8f16a2c5a160f365995ea7c397cb247b41d9b70
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-10-13 22:41:02 +0530

    refactor: formatted `core/domain` module classes with ktlint checks

--- core/domain/src/main/java/com/jetapps/jettaskboard/model/AttachmentModel.kt
+++ core/domain/src/main/java/com/jetapps/jettaskboard/model/AttachmentModel.kt
@@ -1,8 +1,8 @@
 package com.jetapps.jettaskboard.model
 
 data class AttachmentModel(
-  val id: String,
-  val attachmentName: String,
-  val attachmentUrl: String,
-  val attachmentType: String
+    val id: String,
+    val attachmentName: String,
+    val attachmentUrl: String,
+    val attachmentType: String
 )
